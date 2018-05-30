90aac184033cad3294f1e3367d732a8f704fef3d
Vishal Sharma <vsh1819@gmail.com>
2018-05-30 11:31:05 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,5 +9,10 @@ def turn_count(board)
 end
 
 def current_player(board)
-  
+  if(turn_count(board) == 0)
+    return "X"
+  elsif(turn_count(board) % 2 != 0)
+    return "O"
+  else
+    return "X"
 end
